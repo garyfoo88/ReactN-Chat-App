57fb184e3d99f065ca3fb1988d34527b73546a46
garyfoo88 <reid9000200020002000@gmail.com>
2021-06-25 23:35:23 +0800

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoSeM4LFv_idiMAKewArxkP8Kuad1pEOg",
+    authDomain: "react-native-chat-app-7fde3.firebaseapp.com",
+    projectId: "react-native-chat-app-7fde3",
+    storageBucket: "react-native-chat-app-7fde3.appspot.com",
+    messagingSenderId: "1097637569203",
+    appId: "1:1097637569203:web:9a5e00fe1de6cfcaa1c6c6"
+  };
